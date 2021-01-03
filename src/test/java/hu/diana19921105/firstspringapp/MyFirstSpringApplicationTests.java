2ac2e4c60e5c0d78f00bfad1fa037a5b30621dff
Diana Szanto <szanto.diana.1992@gmail.com>
2021-01-03 12:41:42 +0100

    Initial commit

---
+++ src/test/java/hu/diana19921105/firstspringapp/MyFirstSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.diana19921105.firstspringapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFirstSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
